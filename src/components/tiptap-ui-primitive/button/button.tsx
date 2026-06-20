8c65e3f0529be135561fd6abc3bb3d5e81252471
junbyeol <yoonjs0510@gmail.com>
2026-06-20 21:40:43 +0900

    fix: Button에 type 기본값이 적용되고 있지 않던 이슈

--- src/components/tiptap-ui-primitive/button/button.tsx
+++ src/components/tiptap-ui-primitive/button/button.tsx
@@ -69,6 +69,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
           ref={ref}
           data-style={variant}
           data-size={size}
+          type={type}
           {...props}
         >
           {children}
